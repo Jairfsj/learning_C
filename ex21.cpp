5ff435644fb541148dd2f3a5462932396fb27625
Jair Francisco da Silva Junior <61885860+Jairfsj@users.noreply.github.com>
2021-10-21 22:54:31 -0300

    Add files via upload

---
+++ ex21.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  float m_s;
+ 
+  cout<<"\t\t\tCONVERSOR DE VELOCIDADE";
+  cout<<"\n\nDigite a velocidade em M/s: "; 
+  cin>>m_s;
+
+  cout<<"\n\nA velocidade em Km/h eh: "<<m_s*3.6;
+   
+  system("pause"); 
+  return 0;
+
+}
---
+++ ex22.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  float f;
+  cout<<"\t\t\tCONVERSOR DE TEMPERATURA";
+  cout<<"\n\nDgite graus em Fahrenheit : ";
+  cin>>f;
+
+  cout<<"\n\n Em Celsius são : "<<5.0/9*(f-32);
+  
+
+  system("pause");
+  return(0);
+}
