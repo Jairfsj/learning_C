b1aae954114fea255c5ae450e2f35d43e7aebf4f
Jair Francisco da Silva Junior <61885860+Jairfsj@users.noreply.github.com>
2021-11-09 21:42:40 -0300

    Add files via upload

---
+++ ex29.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+  char nome,sexo;
+  int idade ;
+  float salario ;
+  cout<<"Digite seu nome: ";
+  cin>> nome;
+  cout<<"Digite sua idade: ";
+  cin>> idade;
+  cout<<"Digite o seu salário: ";
+  cin>> salario;
+  cout<<"Digite seu sexo F/M: ";
+  cin>> sexo;
+  
+  sexo = m, f;
+
+  if ( sexo == "m"){
+     cout<< "Seu salario é:"<<salario;
+  else
+     if ( sexo == "f" ){
+     cout<<"Sua idade é: "<<idade;
+  };
+   system("pause");
+   return 0;
+}  
