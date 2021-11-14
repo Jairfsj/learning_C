21056e75a7e5907626e5b76b2d1701f535c2302c
Jair Francisco da Silva Junior <61885860+Jairfsj@users.noreply.github.com>
2021-11-14 20:14:43 -0300

    Add files via upload

--- matriz2.cpp
+++ matriz2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
    int mato[4][4],matg[4][4],lin,col;
+   printf ("\n Digite os numeros da matriz original: \n");
    for (lin=0;lin<4;lin++)
      for (col=0;col<4;col++)
      {
