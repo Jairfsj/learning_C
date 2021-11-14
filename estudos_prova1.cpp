6631a639619875eff942d77133d5cddd3ed88a82
Jair Francisco da Silva Junior <61885860+Jairfsj@users.noreply.github.com>
2021-11-14 12:14:58 -0300

    Add files via upload

---
+++ estudos_prova1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int i, num , qnt == 0, vet[10];
+   cin>>num;
+   for (i = 0; i < 10; i++) {
+     if (vet[i] == num) {
+       qnt++;
+   }
+   }
+   {  
+   cout << qnt;
+   }
+   
+   system("pause");
+   return(0);   
+
+}
