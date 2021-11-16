70975703b7bb22f3c0ff3525028dfd5d44a87dce
Jair Francisco da Silva Junior <61885860+Jairfsj@users.noreply.github.com>
2021-11-16 20:38:44 -0300

    Add files via upload

---
+++ questao.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+  int res = 9%2;
+  cout<<res;
+
+
+  system("pause");
+  return 0;
+}
---
+++ questao2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+  int a=3, b=2;
+  cout<<pow((a/b),a-b);
+
+
+  system("pause");
+  return 0;
+}
